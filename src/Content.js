9cee6886fdbd962e54a0eb312fbe93a9b794da22
jasdhiratrevature <jasdhir.singh@revature.com>
2023-05-28 14:42:40 -0400

    06 Use State

--- src/Content.js
+++ src/Content.js
@@ -1,24 +1,38 @@
+import {useState} from 'react';
 
 function Content() {
+  const [name,setName]=useState('Jasdhir');
     const handleNameChange = () => {
         const names=['Ravi','Ramesh','Saurabh'];
         const int=Math.floor(Math.random()*3);
-        return names[int];
+        setName(names[int]);
       }
-      const handleClick=()=>{
-        alert("You Clicked it !")
+      let countA=0;
+
+      const incrementCountA=()=>{
+        countA++;
       }
-      const handleClick2 =(name)=>{
-        alert(`${name} Clicked`)
+
+      const [countB,setCountB]=useState(0);
+      const incrementCountB=()=>{
+        setCountB(countB+1);
       }
+      const decrementCountB=()=>{
+        setCountB(countB-1);
+      }
+     
   return (
     <main>
         <p>
-            Hello {handleNameChange()} !
+            Hello {name} !
         </p>
-        <button onClick={handleClick}>Click It</button>
-        <button onClick={()=>handleClick2('Jasdhir')}>Click Again</button>
-    </main>
+        <button onClick={handleNameChange}>Change Name</button>
+        Count A : {countA}
+        <button onClick={incrementCountA}>Count A</button>
+        Count B : {countB}
+        <button onClick={incrementCountB}>Increase Count B</button>
+        <button onClick={decrementCountB}>Decrease Count B</button>
+        </main>
   )
 }
 

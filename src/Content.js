5e924979b1ed13ed59a00b20c9e5eb10d0fc217f
jasdhiratrevature <jasdhir.singh@revature.com>
2023-05-28 13:25:08 -0400

    02Functional Component

---
+++ src/Content.js
@@ -0,0 +1,17 @@
+
+function Content() {
+    const handleNameChange = () => {
+        const names=['Ravi','Ramesh','Saurabh'];
+        const int=Math.floor(Math.random()*3);
+        return names[int];
+      }
+  return (
+    <main>
+        <p>
+            Hello {handleNameChange()} !
+        </p>
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+
+
+function Footer() {
+    const today=new Date();
+  return (
+    <footer>
+        <p>Copyright &copy; {today.getFullYear()}</p>
+    </footer>
+  )
+}
+
+export default Footer

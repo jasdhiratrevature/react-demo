ffbabec1dd413e70347963eb645116a0ec14a9ff
jasdhiratrevature <jasdhir.singh@revature.com>
2023-05-30 13:38:40 -0400

    13-Fetch-data-from-api

--- src/App.js
+++ src/App.js
@@ -4,23 +4,40 @@ import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const API_URL = 'http://localhost:3500/items';
+  const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=>{
-    localStorage.setItem('shoppinglist', JSON.stringify(items));
-  },[items])
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const listItems = await response.json();
+        setItems(listItems);
+        setFetchError(null);
+      } catch (err) {
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
 
-  
+    setTimeout(() => fetchItems(), 2000);
+  }, [])
+
+
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-  setItems(listItems);
+    setItems(listItems);
   }
 
   const handleSubmit = (e) => {
@@ -46,9 +63,16 @@ function App() {
       <AddItem newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit} />
-        <SearchItem search={search} setSearch={setSearch}/>
-      <Content  items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-        handleCheck={handleCheck} handleDelete={handleDelete} />
+      <SearchItem search={search} setSearch={setSearch} />
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && <Content
+          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />}
+      </main>
       <Footer length={items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -3,13 +3,13 @@ import ItemList from "./ItemList"
 
 function Content({items,handleCheck,handleDelete}) {
  return (
-    <main>
+    <>
       {items.length ? (
        <ItemList items={items} handleCheck={handleCheck} handleDelete={handleDelete}/>
       ) : (
         <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
       )}
-    </main>
+    </>
   )
 }
 
